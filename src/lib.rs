8c22a27aca4621138d438b325cb62811beb99e00
Vincent Berthier <vincent.berthier@posteo.org>
2024-09-03 22:36:45 +0200

    Resources in islands

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,8 @@ mod api;
 mod routes;
 use leptos_meta::{provide_meta_context, Link, Meta, MetaTags, Stylesheet};
 use leptos_router::{
-    components::{ParentRoute, Route, Router, Routes},
-    nested_router::Outlet,
-    path,
+    components::{Outlet, ProtectedParentRoute, Route, Router, Routes},
+    path, SsrMode,
 };
 use routes::{nav::*, stories::*, story::*, users::*};
 #[cfg(feature = "ssr")]
@@ -29,6 +28,16 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
     }
 }
 
+#[server]
+pub async fn check_protection() -> Result<bool, ServerFnError> {
+    Ok(true)
+}
+
+#[server]
+pub async fn get_data(max: u8) -> Result<Vec<u8>, ServerFnError> {
+    Ok((0..max).collect::<Vec<_>>())
+}
+
 #[component]
 pub fn App() -> impl IntoView {
     provide_meta_context();
@@ -37,6 +46,27 @@ pub fn App() -> impl IntoView {
         <Stylesheet id="leptos" href="/pkg/hackernews.css"/>
         <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
         <Meta name="description" content="Leptos implementation of a HackerNews demo."/>
+        <ContextProvider>
+            <IslandRoutes/>
+        </ContextProvider>
+    }
+}
+
+#[island]
+fn ContextProvider(children: Children) -> impl IntoView {
+    let permission = Resource::new(
+        || (),
+        |()| async move { check_protection().await.ok().unwrap_or_default() },
+    );
+    provide_context(permission);
+
+    view! { {children()} }
+}
+
+#[component]
+fn IslandRoutes() -> impl IntoView {
+    let permission = expect_context::<Resource<bool>>();
+    view! {
         <Router>
             <Nav />
             <main>
@@ -44,10 +74,21 @@ pub fn App() -> impl IntoView {
                     <Route path=path!("users/:id") view=User/>
                     <Route path=path!("stories/:id") view=Story/>
                     <Route path=path!("stories") view=Stories/>
-                    // Test parent route
-                    <ParentRoute path=path!("help") view=|| view! { <p>"Help page"</p><Outlet/> }>
-                        <Route path=path!("about") view=|| view! { <p>"About sub page"</p> }/>
-                    </ParentRoute>
+                    <ProtectedParentRoute
+                        path=path!("testing")
+                        condition=move || permission.get()
+                        ssr=SsrMode::Async
+                        redirect_path=|| "/"
+                        view=move || {
+                            view! {
+                                <TestingComponent />
+                                <Outlet />
+                            }
+                        }
+                    >
+
+                        <Route path=path!("") view=|| () />
+                    </ProtectedParentRoute>
                     // TODO allow optional params without duplication
                     <Route path=path!("") view=Stories/>
                 </Routes>
@@ -55,6 +96,96 @@ pub fn App() -> impl IntoView {
         </Router>
     }
 }
+
+#[component]
+fn TestingComponent() -> impl IntoView {
+    view! {
+        <ResourceInComponent/>
+        <ResourceInIsland/>
+        <BlockingResourceInIsland/>
+    }
+}
+
+// Works as expected
+#[component]
+fn ResourceInComponent() -> impl IntoView {
+    let param = RwSignal::new(123_u8);
+    let resource = Resource::new(
+        move || param.get(),
+        move |param| async move { get_data(param).await },
+    );
+
+    view! {
+        <Suspense>
+            <p>"There are " {move || Suspend::new(async move { resource.await.unwrap_or_default().len() })} " data elements in the compoment"</p>
+        </Suspense>
+    }
+}
+
+// Somehow, this writes "There are 123123 data elements in the island"
+#[island]
+fn ResourceInIsland() -> impl IntoView {
+    let param = RwSignal::new(123_u8);
+    let resource = Resource::new(
+        move || param.get(),
+        move |param| async move { get_data(param).await },
+    );
+
+    view! {
+        <Suspense>
+            <p>"There are " {move || Suspend::new(async move { resource.await.unwrap_or_default().len() })} " data elements in the island"</p>
+        </Suspense>
+    }
+}
+
+// Somehow, this writes "There are 123123 data elements in the island"
+#[island]
+fn BlockingResourceInIsland() -> impl IntoView {
+    let param = RwSignal::new(123_u8);
+    let resource = Resource::new_blocking(
+        move || param.get(),
+        move |param| async move { get_data(param).await },
+    );
+
+    view! {
+        <Suspense>
+            <p>"There are " {move || Suspend::new(async move { resource.await.unwrap_or_default().len() })} " data elements in the island"</p>
+        </Suspense>
+    }
+}
+
+// Server panics when loading the page:
+// thread 'tokio-runtime-worker' panicked at /home/vincent/.cargo/registry/src/index.crates.io-6f17d22bba15001f/reactive_graph-0.1.0-beta4/src/owner/stored_value.rs:132:34:
+// Dereferenced SendWrapper<T> variable from a thread different to the one it has been created with.
+// #[island]
+// fn LocalResourceInIsland() -> impl IntoView {
+//     let param = RwSignal::new(123_u8);
+//     let resource = LocalResource::new(move || async move { get_data(param.get()).await });
+//
+//     view! {
+//         <Suspense>
+//             <p>"There are " {move || Suspend::new(async move { resource.await.unwrap_or_default().len() })} " data elements in the island"</p>
+//         </Suspense>
+//     }
+// }
+
+// Panics in the client (unwrap on a none value) + Runtime unreachable
+// #[island]
+// fn ArcLocalResourceInIsland() -> impl IntoView {
+//     let param = RwSignal::new(123_u8);
+//     let resource = ArcLocalResource::new(move || async move { get_data(param.get()).await });
+
+//     let suspend = move || {
+//         let resource = resource.clone();
+//         Suspend::new(async move { resource.await.unwrap_or_default().len() })
+//     };
+
+//     view! {
+//         <Suspense>
+//             <p>"There are " {suspend} " data elements in the island"</p>
+//         </Suspense>
+//     }
+// }
 
 #[cfg(feature = "hydrate")]
 #[wasm_bindgen::prelude::wasm_bindgen]
